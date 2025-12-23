bc8c8adc08bbec554e29c46f5b358ca164fd3254
Angeldust <simon.li@gadz.org>
2025-12-23 22:25:26 +0100

    Pop have names now... and a side menu

--- Micro_Simulation/Agent/agent.gd
+++ Micro_Simulation/Agent/agent.gd
@@ -1,32 +1,32 @@
 extends CharacterBody2D
 
-var length = 1000
+const NAMES = preload("res://Micro_Simulation/Agent/NAMES.gd")
+
+var length = 50
 var Attributes : Dictionary
 
 enum STATUS {STOP,ROAM}
 
-func  init(Id) -> void:
+func  init(Id,spawn_rect) -> void:
 	# Identification
 	self.Attributes["Id"] = Id
+	self.Attributes["Name"] = NAMES.noms.pick_random() + " " + NAMES.prenoms.pick_random()
+	$ASCIILabel.text = str(self.Attributes["Name"])
 	# Postional information
-	self.position.x = int(randf()*DisplayServer.window_get_size()[0])
-	self.position.y = int(randf()*DisplayServer.window_get_size()[1])
+	self.position.x = int(randf()*spawn_rect[0] + DisplayServer.window_get_size()[0] - spawn_rect[0])
+	self.position.y = int(randf()*spawn_rect[1])
 	self.Attributes["Status"] = STATUS.ROAM
 
-func _ready() -> void:
-	$ASCIILabel.text = str(self.Attributes["Id"])
-
-func _on_move_timer_timeout() -> void:
-	$Move_Timer.wait_time = randf_range(0.5,1.5)
-	move_agent()
-	
-func move_agent():
+func _process(_delta: float) -> void:
 	match self.Attributes["Status"] :
 		STATUS.STOP :
 			return
 		STATUS.ROAM :
-			self.velocity = Vector2(randf_range(-length,length),randf_range(-length,length))
 			move_and_slide()
+
+func _on_move_timer_timeout() -> void:
+	$Move_Timer.wait_time = randf_range(1.0,2.5)
+	self.velocity = Vector2(randf_range(-length,length),randf_range(-length,length))
 
 
 func _on_mouse_entered() -> void:
--- main.gd
+++ main.gd
@@ -7,7 +7,7 @@ var N_max = 100
 func _ready() -> void:
 	for i in range(N_max):
 		var myAgent = agent.instantiate()
-		myAgent.init(i)
+		myAgent.init(i,DisplayServer.window_get_size()-Vector2i($UI.size.x,0))
 		$Micro_Simulation.add_child(myAgent)
 
 func _on_timer_timeout() -> void:
